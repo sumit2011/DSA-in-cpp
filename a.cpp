a3c49534f71d0cc92bf93bc667258e81026332b7
Sumit <97377089+sumit2011@users.noreply.github.com>
2022-10-04 11:09:48 +0530

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -42,29 +42,28 @@ void insertlast(int d)
     }
 }
 
-// void deletenode(int d)
-// {
-//     node *ptr=new node();
-//     ptr->data=d;
-//     ptr->next=NULL;
-//     node *temp=head;
-//     while (temp->next != NULL)
-//     {
-//         if(temp->data==d)
-//         {
-//             free(temp);
-//             return;
-//         }
-//         temp=temp->next;
-//     }
-// }
+void deletenode(int p)
+{
+    node *nextnode,*temp;
+    int pos,i=1;
+    pos=p;
+    nextnode=temp=head;
+    while(i<pos)
+    {
+      temp=nextnode;
+      nextnode=nextnode->next;
+      i++;
+    }
+    temp->next=nextnode->next;
+    free(nextnode);
+}
 
 
 void print()
 {
     node *temp=head;
     //temp=head;
-    while (temp->next != NULL)
+    while (temp != NULL)
     {
         cout<<temp->data<<" ";
         temp=temp->next;
@@ -72,21 +71,53 @@ void print()
     
 }
 
+void sortlist()
+{
+    node *ptr , *cpt;
+    ptr = head;
+    int temp;
+    while(ptr->next != NULL)
+    {
+        cpt = ptr->next;
+        while(cpt != NULL)
+        {
+            if(ptr->data > cpt->data)
+            {
+                temp=ptr->data;
+                ptr->data = cpt->data;
+                cpt->data = temp;
+                
+            }
+            cpt=cpt->next;
+        }
+        ptr=ptr->next;
+    }
+}
+
+
 int main()
 {
-    int t,size;
-    cout<<"size of linked list=";
-    cin>>size;
-    for (int i = 0; i < size; i++)
-    {
-        cin>>t;
-        insertlast(t);
-    }
-    
-    // insertfirst(5);
-    // insertfirst(15);
-    // insertlast(10);
-    // insertlast(20);
-    // deletenode(15);
+    // int t,size;
+    // cout<<"size of linked list=";
+    // cin>>size;
+    // for (int i = 0; i < size; i++)
+    // {
+    //     cin>>t;
+    //     insertlast(t);
+    // }
+    // sortlist();
+    insertfirst(5);
+    insertfirst(15);
+    insertlast(10);
+    insertlast(20);
+    cout<<"beforesorting"<<endl;
+    print();
+    cout<<endl;
+    cout<<"after sorting"<<endl;
+    sortlist();
+    print();
+    cout<<endl;
+    cout<<"after deletion"<<endl;
+    deletenode(2);
     print();
 }
