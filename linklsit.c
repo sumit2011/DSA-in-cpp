8c79ef18c0c78bb81c4f61c993d4a64248635440
Sumit <97377089+sumit2011@users.noreply.github.com>
2022-11-09 19:29:25 +0530

    Update linklsit.c

--- linklsit.c
+++ linklsit.c
@@ -1,156 +1,196 @@
-//singly linked list
+// singly linked list
 
+#include <stdio.h>
+#include <stdlib.h>
 
-#include<stdio.h>
-#include<stdlib.h>
-
-//creation of node
+// creation of node
 struct Node
 {
     int data;
     struct Node *next;
-} *head=NULL;
+};
 
-//insertion at begining
+struct Node *head = NULL;
+
+// insertion at begining
 void insertAtBeg(int value)
 {
-    struct Node *newnode;                 
-     newnode = (struct Node*)malloc(sizeof(struct Node));
-     newnode->data=value;                    
-     if (head==NULL)
-     {
-         newnode->next=NULL;
-          head=newnode;
-     }
-     else{
-        newnode->next=head;
-        head=newnode;
-     }
-    printf("\n\n*Inserted successfully at beginning*\n");  
+    struct Node *newnode;
+    newnode = (struct Node *)malloc(sizeof(struct Node));
+    newnode->data = value;
+    if (head == NULL)
+    {
+        newnode->next = NULL;
+        head = newnode;
+    }
+    else
+    {
+        newnode->next = head;
+        head = newnode;
+    }
+    printf("\n\n*Inserted successfully at beginning*\n");
 }
 
-//insertation at end
+// insertation at end
 void insertATend(int value)
 {
-  struct Node *newnode;
-  newnode=(struct Node*)malloc(sizeof(struct Node));
-  newnode->data=value;
-  if(head==NULL)
-  {
-    newnode->next=NULL;
-    head = newnode;
-  }
-  else{
-    struct Node *temp;
-    temp=head;
-    while(temp->next!=NULL)
+    struct Node *newnode;
+    newnode = (struct Node *)malloc(sizeof(struct Node));
+    newnode->data = value;
+    if (head == NULL)
     {
-        temp = temp->next;
+        newnode->next = NULL;
+        head = newnode;
     }
-    temp->next = newnode;
-    newnode->next = NULL;
-  }
-  printf("\n\n*Inserted Successfully in END *\n");
+    else
+    {
+        struct Node *temp;
+        temp = head;
+        while (temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+        temp->next = newnode;
+        newnode->next = NULL;
+    }
+    printf("\n\n*Inserted Successfully in END *\n");
 }
 
-//insertion at any position
-void insertAtPos(int p,int value)
+// insertion at any position
+void insertAtPos(int pos, int value)
 {
-    struct Node *newnode,*temp=NULL;     
-    int pos,i=1;
-    newnode= (struct Node*)malloc(sizeof(struct Node));
-    pos=p;
-    newnode->data=value;
-    temp=head;
-    while(i<pos-1)
+    struct Node *newnode;
+    newnode = (struct Node *)malloc(sizeof(struct Node));
+    newnode->data = value;
+
+    struct Node *temp = NULL;
+    int i = 1;
+    temp = head;
+    while (i < pos - 1)
     {
-      temp = temp->next;
-      i++;
+        temp = temp->next;
+        i++;
     }
     newnode->next = temp->next;
-    temp->next = newnode; 
- }
+    temp->next = newnode;
+}
 
-//deletion at begining
+// deletion at begining
 void deletatBeg()
 {
     struct Node *temp;
-    temp=head;
-    head=head->next;
+    temp = head;
+    head = head->next;
     free(temp);
 }
 
-//deletion at end
+// deletion at end
 void deletatEnd()
 {
-    struct Node *temp,*prev;
-    prev=temp=head;
-    while(temp->next!=NULL)
+    struct Node *temp, *prev;
+    prev = temp = head;
+    while (temp->next != NULL)
     {
-        prev=temp;
-        temp=temp->next;
+        prev = temp;
+        temp = temp->next;
     }
-    prev->next=NULL;
+    prev->next = NULL;
     free(temp);
 }
 
-//deletion at any position
-void deletatPos(int p)
+// deletion at any position
+void deletatPos(int pos)
 {
-struct Node *nextnode,*temp;
-int pos,i=1;
-pos=p;
-nextnode=temp=head;
-while(i<pos)
-{
-    temp=nextnode;
-    nextnode=nextnode->next;
-    i++;
-}
-temp->next=nextnode->next;
-free(nextnode);
+    struct Node *nextnode, *temp;
+    int i = 1;
+    nextnode = temp = head;
+    while (i < pos)
+    {
+        temp = nextnode;
+        nextnode = nextnode->next;
+        i++;
+    }
+    temp->next = nextnode->next;
+    free(nextnode);
 }
 
-//sorting of linked list
+// sorting of linked list
 void sortlist()
 {
-    struct Node *ptr , *cpt;
+    struct Node *ptr, *cpt;
     ptr = head;
     int temp;
-    while(ptr->next != NULL)
+    while (ptr->next != NULL)
     {
         cpt = ptr->next;
-        while(cpt != NULL)
+        while (cpt != NULL)
         {
-            if(ptr->data > cpt->data)
+            if (ptr->data > cpt->data)
             {
-                temp=ptr->data;
+                temp = ptr->data;
                 ptr->data = cpt->data;
                 cpt->data = temp;
-                
             }
-            cpt=cpt->next;
+            cpt = cpt->next;
         }
-        ptr=ptr->next;
+        ptr = ptr->next;
     }
 }
 
-//printing of linked list
+void reverse()
+{
+    struct Node *current = head;
+    struct Node *prev = NULL, *next = NULL;
+
+    while (current != NULL)
+    {
+        next = current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
+    }
+    head = prev;
+}
+
+// printing of linked list
 void print()
 {
     struct Node *temp;
-    temp=head;
-    while(temp!=NULL)
+    temp = head;
+    while (temp != NULL)
     {
-    printf("%d-->",temp->data);
-    temp=temp->next;
+        printf("%d-->", temp->data);
+        temp = temp->next;
     }
     printf("NULL");
 }
 
-//main function
+// length of the linked
+int getlength()
+{
+    int len = 0;
+    struct Node *temp = head;
+    while (temp != NULL)
+    {
+        len++;
+        temp = temp->next;
+    }
+    printf("%d", len);
+}
+
+// main function
 int main()
 {
+    // int a;
+    // printf("how many elements do you want enter= ");
+    // scanf("%d", &a);
+    // for (int i = 0; i < a; i++)
+    // {
+    //     int data;
+    //     scanf("%d", &data);
+    //     insertATend(data);
+    // }
+    // print();
     insertAtBeg(34);
     insertAtBeg(45);
     insertAtBeg(72);
@@ -161,7 +201,9 @@ int main()
     insertATend(55);
     insertATend(13);
     insertATend(41);
-    insertAtPos(6,8992);
+    insertAtPos(6, 8992);
+    print();
+    printf("\n");
     deletatBeg();
     deletatEnd();
     deletatPos(3);
@@ -169,5 +211,13 @@ int main()
     printf("\n");
     sortlist();
     print();
+    insertATend(58);
+    print();
+    printf("\n");
+    reverse();
+    print();
+    printf("\n\n");
+    printf("length: ");
+    getlength();
     return 0;
 }
