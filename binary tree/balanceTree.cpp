df0cb3b01ed42a4398c7d35c74fd9b05c8922518
no0ne <sumit@n4ksum>
2023-10-19 21:58:33 +0530

    uploads

---
+++ binary tree/balanceTree.cpp
@@ -0,0 +1,70 @@
+//check for balanced tree
+//height(leftsubtree)-height(rightsubtree)<=1
+
+
+
+class Solution{
+    private:
+        int height(struct Node* node){
+            //base case
+            if(node==NULL){
+                return 0;
+            }
+            int left = height(node->left);
+            int right = height(node->right);
+            int ans = max(left,right)+1;
+            return ans;
+        }
+
+
+    public:
+
+    bool isBalanced(Node *root){
+        //base case
+        if(root==NULL){
+            return true;
+        }
+
+        bool left = isBalanced(root->left);
+        bool right = isBalanced(root->right);
+
+        bool diff = abs (height(root->left) - height(root->right)) <= 1;
+
+        if(left && right && diff){
+            return 1;
+        }
+        else{
+            return false;
+        }
+    }
+
+    pair<bool, int> isBalancedFast(Node* root){
+        if(root == NULL){
+            pair<bool,int> p = make_pair(true, 0);
+            return p;
+        }
+        
+        pair<int,int> left = isBalancedFast(root->left);
+        pair<int,int> right = isBalancedFaced(root->right);
+
+        bool leftAns = left.first;
+        bool rightAns = right.first;
+
+        bool diff = abs(left.second - right.second) <= 1;
+
+        pair<bool,int> ans;
+        ans.second = max(left.second , right.second) + 1;
+
+        if(leftAns && rightAns && diff){
+            ans.first = true;
+        }
+
+        else{
+            ans.first = false;
+        }
+        return ans;
+    }
+    bool isBalanced(Node *root){
+        return isBalanced(root).first;
+    }
+};
